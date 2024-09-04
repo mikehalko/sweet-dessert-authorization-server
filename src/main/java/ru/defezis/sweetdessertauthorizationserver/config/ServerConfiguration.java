885531281b6e8bd8f246266dff5fb1eac8c218c6
Artem Mikhalko <amikhalko@geosteering.ru>
2024-09-04 15:19:40 +0500

    configuration

--- src/main/java/ru/defezis/sweetdessertauthorizationserver/config/ServerConfiguration.java
+++ src/main/java/ru/defezis/sweetdessertauthorizationserver/config/ServerConfiguration.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
@@ -19,7 +19,9 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.KeyPair;
@@ -42,6 +44,11 @@ public class ServerConfiguration {
     }
 
     @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().issuer("http://127.0.0.1:9000").build();
+    }
+
+    @Bean
     public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId("sweet-dessert-client")
@@ -50,19 +57,14 @@ public class ServerConfiguration {
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .redirectUri("http://127.0.0.1:9090/login/oauth2/code/sweet-dessert-client")
-                .scope("writeDessert")
-                .scope("deleteDessert")
+                .scope("writeDesserts")
+                .scope("deleteDesserts")
+                .scope("openid")
                 .scope(OidcScopes.OPENID)
-                .clientSettings(clientSettings -> clientSettings.requireUserConsent(true))
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                 .build();
 
         return new InMemoryRegisteredClientRepository(registeredClient);
-    }
-//  http://localhost:9000/oauth2/authorize?response_type=code&client_id=sweet-dessert-client&redirect_uri=http://127.0.0.1:9090/login/oauth2/code/sweet-dessert-client&-scope=writeDessert+deleteDessert
-
-    @Bean
-    public ProviderSettings providerSettings() {
-        return new ProviderSettings().issuer("http://localhost:9000");
     }
 
     @Bean
@@ -94,8 +96,20 @@ public class ServerConfiguration {
     }
 }
 
-
 /*
-localhost:9000/oauth2/token -H"Content-type: application/x-www-form-urlencoded" -d"grant_type=authorization_code" -d"redirect_uri=http://127.0.0.1:9090/login/oauth2/code/sweet-dessert-client" -d"code=b33755a3-6791-4006-b9d5-7c00d6edc77d" -u sweet-dessert-client:secret
+http://localhost:9000/oauth2/authorize?response_type=code&client_id=sweet-dessert-client&redirect_uri=http://127.0.0.1:9090/login/oauth2/code/sweet-dessert-client&scope=writeDessert+deleteDessert
+
+localhost:9000/oauth2/token -H"Content-type: application/x-www-form-urlencoded" -d"grant_type=authorization_code" -d"redirect_uri=http://127.0.0.1:9090/login/oauth2/code/sweet-dessert-client" -d"code=???" -u sweet-dessert-client:secret
+
+
+{
+	"access_token": "eyJraWQiOiJhMTBjOTJiYi1iMTUxLTRlM2ItYmZmMi1jYjAwM2E0MTZhNWEiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImF1ZCI6InN3ZWV0LWRlc3NlcnQtY2xpZW50IiwibmJmIjoxNzI0NDkzOTY4LCJzY29wZSI6WyJkZWxldGVEZXNzZXJ0Iiwid3JpdGVEZXNzZXJ0Il0sImlzcyI6Imh0dHA6Ly8xMjcuMC4wLjE6OTAwMCIsImV4cCI6MTcyNDQ5NDI2OCwiaWF0IjoxNzI0NDkzOTY4fQ.pDQ3KB5Wl0OKJWE0p4lzglC9HWQZK4sEHhKJ74hTlinENP-iVgBZ0WpvQh9GcOZDHO5Zf1HR8EoFstk4zY1Lg84jObtineGKd1K30u3yGoZE9XK609gYRpydGulk211v7tP4B86W4bmgBPA9DJov36D8FzMnpk36BUiYo1at2uMzq7YPNDTZu2xqq1tV6PNw0vyMo3VHoBI8XNlCS8vmMtB7A5q-5onl0O-1wEB9EecDH-DLzT1HZmh6HEFBZf5AE862igNNyLcvxmIGvEfsg6SwuzlS9-IxhAs4tH-UchKnxgkpMaZ0s_M-6f9AMATZtEVaB7QAepNTUA-oiD4oZA",
+	"refresh_token": "fQxTz-5STUD09ehqQ2ePn9rWtT46gsWKFHx--DQRdrczDVoJ9JIA5Y0fmCMPYtjjpxsEccRjrVJZVCjcg6O0vbnE2J3nijlSLfw9iazNjFsik_LmdGNw2rOaCBDLSeDh",
+	"scope": "deleteDessert writeDessert",
+	"token_type": "Bearer",
+	"expires_in": 299
+}
+
+curl localhost:9000/oauth2/token -H"Content-type: application/x-www-form-urlencoded" -d"grant_type=refresh_token&refresh_token=fQxTz-5STUD09ehqQ2ePn9rWtT46gsWKFHx--DQRdrczDVoJ9JIA5Y0fmCMPYtjjpxsEccRjrVJZVCjcg6O0vbnE2J3nijlSLfw9iazNjFsik_LmdGNw2rOaCBDLSeDh" -u sweet-dessert-client:secret
 
  */
